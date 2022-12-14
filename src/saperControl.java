6c8810aafe7cdadbed48ccfa645b0bafcba817c8
iNQU1SITOR <sankevich2003@mail.ru>
2022-12-14 13:05:42 +0300

    no

--- src/saperControl.java
+++ src/saperControl.java
@@ -33,8 +33,6 @@ public class saperControl implements Initializable {
    private ArrayList<String> addBombNumb() {
        ArrayList<String> bombList = new ArrayList<>(Const.NUMB_OF_BOMBS);
        int i = 0;
-
-
        while (i < Const.NUMB_OF_BOMBS) {
            int bombNumb = Random.getRandNumber();
            while (bombList.contains("button"+ bombNumb)) {
